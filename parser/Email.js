8520b4251c1adf3eee4320f80e5e325a7c52cb67
sp4rk2 <sp4rk2sp4rk2@gmail.com>
2019-03-29 14:57:32 +0000

    More progress on the parser.

--- parser/Email.js
+++ parser/Email.js
@@ -9,33 +9,59 @@ let Email = function(str) {
 Email.attributes = {};
 
 Email.attributes.returnPath = function(email, str) {
-    let match = str.match(/(?=Return-Path:\s)(.*)/g);
-    if (match.length > 1) console.error("FOUND MULTIPLE FOR RETURN PATH");
-    else email["Return-Path"] = match[0].split(": ")[1];
+    let match = str.match(/Return-Path: .(.*[\n\r](\t.*[\n\r])*)/g);
+    if (match == null) console.log("NONE FOUND FOR Return-Path ATTRIBUTE");
+    if (match.length > 1) console.error("FOUND MULTIPLE FOR Return-Path ATTRIBUTE");
+    else email["Return-Path"] = match[match.length - 1].split(": ")[1];
 }
 
 Email.attributes.xOriginalTo = function(email, str) {
-
+    let match = str.match(/X-Original-To: .(.*[\n\r](\t.*[\n\r])*)/g);
+    if (match == null) console.log("NONE FOUND FOR X-Original-To Attribute");
+    if (match.length > 1) {
+        console.error("FOUND MULTIPLE FOR X-Original-To ATTRIBUTE");
+        // console.log(str);
+    } else email["X-Original-To"] = match[match.length - 1].split(": ")[1];
 }
 
 Email.attributes.deliveredTo = function(email, str) {
-    
+    let match = str.match(/Delivered-To: .(.*[\n\r](\t.*[\n\r])*)/g);
+    if (match == null) console.log("NONE FOUND FOR Delivered-To ATTRIBUTE");
+    if (match.length > 1) {
+        console.error("FOUND MULTIPLE FOR Delivered-To ATTRIBUTE");
+        // console.log(str);
+    } else email["Delivered-To"] = match[match.length - 1].split(": ")[1]; 
 }
 
 Email.attributes.received = function(email, str) {
-    
+
 }
 
 Email.attributes.from = function(email, str) {
-    
+    let match = str.match(/From: .(.*[\n\r](\t.*[\n\r])*)/g);
+    if (match == null) console.log("NONE FOUND FOR From Attribute");
+    if (match.length > 1) {
+        console.error("FOUND MULTIPLE FOR From ATTRIBUTE");
+        // console.log(str);
+    } else email["From"] = match[match.length - 1].split(": ")[1];
 }
 
 Email.attributes.subject = function(email, str) {
-    
+    let match = str.match(/Subject: .(.*[\n\r](\t.*[\n\r])*)/g);
+    if (match == null) console.log("NONE FOUND FOR Subject Attribute");
+    if (match.length > 1) {
+        console.error("FOUND MULTIPLE FOR Subject ATTRIBUTE");
+        // console.log(str);
+    } else email["Subject"] = match[match.length - 1].split(": ")[1];
 }
 
 Email.attributes.to = function(email, str) {
-    
+    let match = str.match(/To: .(.*[\n\r](\t.*[\n\r])*)/g);
+    if (match == null) console.log("NONE FOUND FOR To Attribute");
+    if (match.length > 1) {
+        console.error("FOUND MULTIPLE FOR To ATTRIBUTE");
+        console.log(str);
+    } else email["To"] = match[match.length - 1].split(": ")[1];
 }
 
 Email.attributes.contentType = function(email, str) {
--- parser/parser.js
+++ parser/parser.js
@@ -1,70 +1,35 @@
 const config = require("../config.json");
-const dataSet = "../data-set" + config.dataSet;
-let Email = require("../parser/Email.js");
-
-let test = `From exchange-robot@paypal.com  Sun Nov 28 17:22:23 2004
-Return-Path: <exchange-robot@paypal.com>
-X-Original-To: username@domain.com
-Delivered-To: username@domain.com
-Received: from mail2.domain.com (mail2.domain.com [192.168.64.8])
-	by spanky.domain.com (Postfix) with ESMTP id 9CE11537B7D
-Content-Type: multipart/mixed;
-	boundary="----=_NextPart_000_0000_01C4D5AE.9231CEA0"
-X-Priority: 3
-X-MSMail-Priority: Normal
-X-Mailer: Microsoft Outlook Express 6.00.2600.0000
-
-<p><i>Congratulations! PayPal has successfully charged $175 to your credit
-card. Your order tracking number is A866DEC0, and your item will be shipped
-within three business days. </i></p>
-<p><i>Thank you for using PayPal.</i></p>
-</body>
-</html>
-
-------=_NextPart_000_0000_01C4D5AE.9231CEA0--
-
-
-From aw-confirm@ebay.com  Sun Nov 28 17:24:56 2004
-Return-Path: <aw-confirm@ebay.com>
-X-Original-To: username@domain.com
-Delivered-To: username@domain.com
-Received: from mail2.domain.com (mail2.domain.com [192.168.64.8])
-Message-ID: <JDXSYRKPDISQIRUBXXMCHBC@goldenmail.ru>
-From: "eBay.com" <aw-confirm@ebay.com>
-Reply-To: "eBay.com" <aw-confirm@ebay.com>
-X-Priority: 1
-X-MSMail-Priority: High               
-X-UID: 5
-
-
-From aw-confirm@ebay.com  Sun Nov 28 20:08:58 2004
-Return-Path: <aw-confirm@ebay.com>
-X-Original-To: username@domain.com
-Delivered-To: username@domain.com
-Received: from mail2.domain.com (mail2.domain.com [192.168.64.8])
-	by spanky.domain.com (Postfix) with ESMTP id 61054538CD9
-	for <username@domain.com>; Sun, 28 Nov 2004 20:08:53 -0500 (EST)
-Received: from 62.193.237.52 (wpc0865.amenworld.com [62.193.237.52])
-	by mail2.domain.com (Postfix) with SMTP id 13EF318CBCB
-	for <username@domain.com>; Sun, 28 Nov 2004 13:12:35 -0500 (EST)
-Received: from 112.76.224.216 by ; Sun, 28 Nov 2004 21:18:05 -0200`
-
+const dataSet = "data-set/" + config.dataSet;
+const fs = require("fs");
+const Email = require("../parser/Email.js");
 
 let parser = {};
 
 
 parser.seperateEmails = function(str) {
     let rawEmailArray = 
-        str.match(/(From\s.*\s\d{4}\n)(.|\n)*?(?=(From\s.*\s\d{4}\n|$(?![\r\n])))/g);
+        str.match(/(From\s.*\s\d{4})(.|[\n\r])*?(?=(From\s.*\s\d{4}|$(?![\r\n])))/g);
     return rawEmailArray;
 }
 
 
 parser.init = function() {
-    let results = parser.seperateEmails(test);
-    let email = new Email(results[0]);
+    let content = fs.readFileSync(dataSet, "utf8");
+    let rawEmailArray = parser.seperateEmails(content);
+    console.log("LOADED " + rawEmailArray.length + " RAW EMAILS");
 
-    console.log(email);
+    let emailArray = [];
+
+    rawEmailArray.forEach(function(element) {
+        emailArray.push(new Email(element));
+    });
+
+    
+
+    // for (let i = 0; i < 100; i++) {
+    //     console.log(emailArray[i]);
+    // }
+
 };
 
 
